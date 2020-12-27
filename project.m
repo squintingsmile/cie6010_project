c8158b166f9a7e7589f4a5a6364b33484163dd15
squintingsmile <squintingsmile@gmail.com>
2020-12-27 22:02:42 +0800

    Added 3d surface plot function and testing

--- project.m
+++ project.m
@@ -1,8 +1,8 @@
-n = 3; % Size of the matrix
-length = 1 / (n -1);
-iter_count = 100;
+n = 20; % Size of the matrix
+length = 1 / (n - 1);
+iter_count = 10000;
 gradient_diff = 1e-3;
-step_size = 0.1;
+step_size = 0.2;
 
 total_graph = zeros(n); % Storing the boundary and values of xi,j
 active_mask = zeros(n); % Indicating which points are not boundary 
@@ -11,11 +11,11 @@ constraint_graph = zeros(n); % Indicating the inequality contraint on each
                  % points to be -Inf and the constraint points to be their
                  % constraints
 
-total_graph(2, 2) = 10;
 
 for i=2:n-1
     for j=2:n-1
         active_mask(i,j) = 1;
+        total_graph(i,j) = 10;
     end
 end
 
@@ -25,15 +25,23 @@ r3 = @(x, y)1/2 - abs(y - 1/2);
 r4 = @(x, y)(1 + exp(x * y))^(-1);
 r5 = @(x, y)1 + asin(-1 + 2 * sqrt(x * y));
 
+total_graph = set_boundary(r1, total_graph, n);
+
 plot_boundary(r1);
+[X,Y] = meshgrid(0:length:1,0:length:1);
 
 for iter=1:iter_count
+    if mod(iter, 100) == 0
+        fprintf("iteration count: %d\n", iter);
+    end
     num_grad_mat = get_graph_gradient(total_graph, active_mask, constraint_graph, n, length, gradient_diff);
 %     num_grad_mat
     step = step_size * num_grad_mat;
     total_graph = total_graph - step; 
 %     total_graph
 end
+
+surf(X,Y,total_graph);
 
 % The arrangement of x1, x2, x3, and x4 is as follows
 % The length is the length of the square
@@ -79,6 +87,7 @@ end
 % the gradient w.r.t xi 
 function numerical_grad = get_graph_gradient(total_graph, active_mask, constraint_graph, size, length, gradient_diff)
     numerical_grad = zeros(size);
+    triag_areas_tmp = zeros(2*size,size); % Storing the areas of the triangles
     for i=1:size
         for j=1:size
             if active_mask(i,j) == 1
@@ -122,4 +131,18 @@ function status = plot_boundary(eval_func)
     plot3(x2, y1, Z(3,:));
     plot3(x2, y3, Z(4,:));
     hold off;
+end
+
+function graph = set_boundary(eval_func, total_graph, size)
+    resolution = 1 / (size - 1);
+    for x=1:size
+        total_graph(x, 1) = eval_func((x - 1) * resolution, 0);
+        total_graph(x, size) = eval_func((x - 1) * resolution, 1);
+    end
+    
+    for y=1:size
+        total_graph(1, y) = eval_func(0, (y - 1) * resolution);
+        total_graph(size, y) = eval_func(1, (y - 1) * resolution);
+    end
+    graph = total_graph;
 end
